a2b101dd0b2706a32f39e383e8837b5927cedf2e
Michel <s0577677@htw-berlin.de>
2021-04-20 19:30:24 +0200

    ShowOptionDialog eingefügt

--- SoSe20/src/Aufgabe1/Meldung.java
+++ SoSe20/src/Aufgabe1/Meldung.java
@@ -19,7 +19,34 @@ public class Meldung
 		//Aufruf der statischen Methode showMessageDialog()
 		JOptionPane.showMessageDialog(null, "Haben Sie noch einen schönen Tag", "Abschied", JOptionPane.WARNING_MESSAGE);
 		
-		
+		//Aufruf der statischen showOptionDialog()
+		Object[] options = {"FIW", "AI", "IMI", "WI", "Alle"};
+			
+			int selected = JOptionPane.showOptionDialog(null, "Bester Studiengang?", "Auswahl", 
+					JOptionPane.DEFAULT_OPTION, 
+					JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+			if(selected==0)
+			{
+				System.out.println("FIW");
+			}
+			if(selected==1)
+			{
+				System.out.println("AI");
+			}
+			if(selected==2)
+			{
+				System.out.println("IMI");
+			}
+			if(selected==3)
+			{
+				System.out.println("WI");
+				
+			}
+			else
+			{
+				System.out.println("alle");
+				
+			}
 		
 		System.exit(0);
 				
