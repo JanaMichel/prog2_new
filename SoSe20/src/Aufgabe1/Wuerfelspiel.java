ce7e6d1a7e2ae85b2b8909de263a087758ce60ab
Michel <s0577677@htw-berlin.de>
2021-04-20 21:53:04 +0200

    implementierung der Lösung von Lilit, morgen wird eigenes Würfelspiel erarbeitet

--- SoSe20/src/Aufgabe1/Wuerfelspiel.java
+++ SoSe20/src/Aufgabe1/Wuerfelspiel.java
@@ -1,10 +1,24 @@
 package Aufgabe1;
 
-public class Wuerfelspiel {
+import javax.swing.JOptionPane;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Wuerfelspiel
+	{
+		public static void main(String [] bla)
+		{
+			//Eingabe des Namen, ShowOptionDialog
+			String name = JOptionPane.showInputDialog(null, "Geben Sie Ihren Namen ein", "Spielereingabe", JOptionPane.PLAIN_MESSAGE);
+			int option = JOptionPane.showConfirmDialog(null, name + ", wollen Sie weiter würfeln?", "Weiter würfeln?", JOptionPane.YES_NO_OPTION);
+			if(option==0)
+			{
+				System.out.println(name + " ist am Zug");
+			}
+			if(option==1)
+			{
+				System.out.println("Das Spiel ist beendet");
+			}
+			
+			System.exit(0);		
+		}
 	}
 
-}
