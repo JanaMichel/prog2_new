ea5dc3d48d1542e8857abe884c6927ef8ec6a3b8
Michel <s0577677@htw-berlin.de>
2021-04-20 20:22:50 +0200

    Eingabe des Namen über ShowInputDialog und Ja,Nein ConfirmDialog verbunden

--- SoSe20/src/Aufgabe1/Meldung.java
+++ SoSe20/src/Aufgabe1/Meldung.java
@@ -39,6 +39,17 @@ public class Meldung
 				System.out.println("beide");
 			}
 			
+		//Eingabe des Namen, ShowOptionDialog
+		String name = JOptionPane.showInputDialog(null, "Geben Sie Ihren Namen ein", "Spielereingabe", JOptionPane.PLAIN_MESSAGE);
+		int option = JOptionPane.showConfirmDialog(null, name + ", wollen Sie weiter würfeln?", "Weiter würfeln?", JOptionPane.YES_NO_OPTION);
+		if(option==0)
+		{
+			System.out.println(name + " ist am Zug");
+		}
+		if(option==1)
+		{
+			System.out.println("Das Spiel ist beendet");
+		}
 		
 		System.exit(0);
 				
