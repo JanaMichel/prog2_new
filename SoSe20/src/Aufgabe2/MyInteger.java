97e59529f8c53c0d11fb054744a20721ca1a2f6c
Michel <s0577677@htw-berlin.de>
2021-05-02 12:39:20 +0200

    die Methoden charAtIndex und charAtIndexValue erzeugt, string value noch nicht fertig

--- SoSe20/src/Aufgabe2/MyInteger.java
+++ SoSe20/src/Aufgabe2/MyInteger.java
@@ -12,17 +12,20 @@ public class MyInteger
 	{
 		switch(zahl)
 		{
-			case 48 : System.out.println("0"); break;
-			case 49 : System.out.println("1"); break;
-			case 50 : System.out.println("2"); break;
-			case 51 : System.out.println("3"); break;
-			case 52 : System.out.println("4"); break;
-			case 53 : System.out.println("5"); break;
-			case 54 : System.out.println("6"); break;
-			case 55: System.out.println("7"); break;
-			case 56: System.out.println("8"); break;
-			case 57: System.out.println("9"); break;
-			default: System.out.println("Keine Zahl");
+			case 43 : System.out.print("+"); break;
+			case 45 : System.out.print("-"); break;
+			case 46 : System.out.print("."); break;
+			case 48 : System.out.print("0"); break;
+			case 49 : System.out.print("1"); break;
+			case 50 : System.out.print("2"); break;
+			case 51 : System.out.print("3"); break;
+			case 52 : System.out.print("4"); break;
+			case 53 : System.out.print("5"); break;
+			case 54 : System.out.print("6"); break;
+			case 55: System.out.print("7"); break;
+			case 56: System.out.print("8"); break;
+			case 57: System.out.print("9"); break;
+			default: System.out.print("Keine Zahl");
 		}
 	}
 	
@@ -38,46 +41,54 @@ public class MyInteger
 		this.value = Integer.parseInt(s);
 	}
 	
-	public static boolean chatAtIndex(String message)
+	public static boolean charAtIndex(String message)
 	{
 		for (int i = 0; i < message.length(); i++) 
 		{
 			int b = message.charAt(i);
-			if(b >=48 && b <=57)
+			if(b >=48 && b <= 57 || b == 45 || b == 46 || b == 43)
 			{
-				printZahl(b);
 				return true;
 			}
 		}
 		return false;
 	}
 	
+	public static int charAtIndexValue(String message)
+	{
+		int zahl = 0;
+		for (int i = 0; i < message.length(); i++) 
+		{
+			int b = message.charAt(i);
+			char d = (char) b;
+			if(b >=48 && b <= 57 || b == 45 || b == 46 || b == 43)
+			{
+				
+			}
+		}
+		return zahl;
+	}
+	
 	public static int inputInt(int min, int max, String message)
     {
         boolean eingabeOk = false;
         int zahl = 0;
-     
         // String message = "Geben Sie eine Zahl ein : ";
         while(!eingabeOk)
         {
             String eingabe1 = JOptionPane.showInputDialog(message);
-            for (int i = 0; i < eingabe1.length(); i++) {
-				
-			}
-            try {
-                zahl = Integer.valueOf(eingabe1);
-                if(zahl>=min && zahl<=max)
-                {
-                    eingabeOk = true;
-                }
-                else
-                {
-                    message = "Zahl muss zwischen " + min + " und " + max + " liegen!";
-                }
-            } 
-            catch (NumberFormatException e) 
+            if(charAtIndex(eingabe1)==true);
             {
-                message = "Eingabe war keine Zahl! Bitte Zahl eingeben";
+            	try 
+            	{
+            		zahl = charAtIndexValue(eingabe1);            		
+            		eingabeOk = true;
+            	}
+             
+            	catch (NumberFormatException e) 
+            	{
+            		message = "Eingabe war keine Zahl! Bitte Zahl eingeben";
+            	}
             }
         }
         return zahl;
@@ -108,8 +119,9 @@ public class MyInteger
 		return test;
 	}*/
 	
-	/*public static string valueOf(int value)
+	public static String valueOf(int value)
 	{
-		return new MyInteger(value);
-	}*/
+		String message = "";
+		return message;
+	}
 }
