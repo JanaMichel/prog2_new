b4abc665cfce551c77997a9c5d4c34aae636f5ba
Michel <s0577677@htw-berlin.de>
2021-05-05 10:02:21 +0200

    Neues Paket Vorlesung4 und neue Klasse ListenUndMengen

---
+++ SoSe20/src/Vorlesung4/ListenUndMengen.java
@@ -0,0 +1,23 @@
+package Vorlesung4;
+
+/*
+ *Collections: ähnlich wie Arrays, viele Elemente speichern
+ *keine feste Länge
+ *wir können nur Referenztypen speichern, kein int, boolean, char, double, ...
+ *Elemente versch. Typs speicherbar --> aber fehleranfällig, deshalb Typisieren
+ *Arten von Collections: 
+ * --> List (wie Arrays)
+ * --> Set (keine Dopplung, laut boolean)
+ * --> Stack 
+ * --> Queue (warteschlange, erste rein, letzte raus)
+ * --> Map 2-Spaltige Tabellen, linke schlüssel (postleitzahl), rechte Werte-Paare (ortschaft)
+ */
+
+public class ListenUndMengen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
